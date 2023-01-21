1275217a54d55a2211ed5a565dd43ba698f7af99
IWANDETO <wandettoian@gmail.com>
2023-01-21 11:42:49 +0300

    functions

---
+++ numbers.py
@@ -0,0 +1,2 @@
+#This is a python file used to demonstrate math operations in python
+3 + 4
---
+++ variables.py
@@ -0,0 +1,16 @@
+print("This is a python file to demonstrate variables in Python")
+
+lang_name = "C"
+lang_num = 1
+print("There are 3 types of variables learned so far: being Strings, Numbers and Boolean Expressions")
+difficulty = True
+print("Learning " + lang_name + " was not easy")
+print("This was language number ", lang_num)  
+print("When Learning C, its", difficulty, " to say its hard as it is a low level language")
+
+lang_name = "Python"
+lang_num = 2
+difficulty = False
+print("Learning " + lang_name + " was easy")
+print("This was language number ", lang_num)  
+print("When Learning C, its", difficulty, " to say its hard as it is a high level language")
